6c7929fa5efd4f57cd8f3fc9493fbf6b5b5196fe
andres <andresdavid.ortega@gmail.com>
2024-12-07 17:18:58 -0500

    panel del cliente creado, falta crear componente carrito

--- cliente_tienda/src/pages/cliente/ClientPage.jsx
+++ cliente_tienda/src/pages/cliente/ClientPage.jsx
@@ -1,6 +1,18 @@
 
-export default function ClientPage() {
-    return (
-      <div>ClientPage</div>
-    )
-  }
+import { ClientList } from "../../components/cliente/ClientList"
+import { useParams } from "react-router-dom"
+import { Navigation } from '../../components/cliente/Navigation'
+
+export function ClientPage() {
+  const params = useParams(); // Obtén todos los parámetros como un objeto
+  const { criteria, value } = params;
+  
+  return (
+    <div>
+      <Navigation />
+      <div className='container mx-auto mt-4'><ClientList searchCriteria={criteria} searchValue={value}></ClientList></div>
+
+    </div>
+
+  )
+}
--- cliente_tienda/src/pages/login/LoginFormPage.jsx
+++ cliente_tienda/src/pages/login/LoginFormPage.jsx
@@ -36,7 +36,6 @@ export function LoginFormPage() {
   
 
   return (
-<<<<<<< HEAD
     <div className="max-w-xl mx-auto mt-10">
       <h1 className="text-center text-3xl font-bold text-gray-800 mb-6">Log in</h1>
   
@@ -83,24 +82,6 @@ export function LoginFormPage() {
             </p>
           </div>
         </div>
-=======
-    <div className='max-w-xl mx-auto mt-10'>
-
-      <div className='text-xl'>Login</div>
-      <form className='mt-10' onSubmit={onSubmit}>
-
-        <input className='bg-zinc-700 p-3 rounded-lg block w-full mb-3' type="text" name="username" placeholder="username" {...register("username", { required: true })} />
-
-
-        <input className='bg-zinc-700 p-3 rounded-lg block w-full mb-3' type='password' name="password" placeholder="password" {...register("password", {required:true })}/>
-
-     
-
-        <button className='bg-indigo-500 p-3 rounded-lg  w-48 mt-3' type="submit">Login</button>
-
-        <p className='mt-10'>No tienes cuenta? <Link className='text-xl' to='/register-user'>Registrarse</Link></p>
-      
->>>>>>> b69ba0b (captcha completado)
       </form>
     </div>
   );
